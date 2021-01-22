c96bcdb5ad31b8197bb3f5ec9881644b597dfb15
Brikend Rama <brikend@huddly.com>
2021-01-22 12:40:42 +0100

    meson: Add preliminary support for cross builds …
    This currently works for compiling glib for aarch64 on x86_64,
    but probably not for anything else yet. But it should be simpler
    to add more architectures when needed than the situation today.
    
    Original Author(s): Kristian Jerpetjøn, Kristoffer Ellersgaard Koch

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=load_version(),  # + ".rc1",
+    version=load_version() + "-huddly",
 
     description='Conan C/C++ package manager',
     long_description=generate_long_description_file(),
