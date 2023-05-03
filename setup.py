b5be976644a7519949621d11138826c41f64a5ab
trymnordal <trym1994@gmail.com>
2023-05-03 12:43:12 +0200

    setup.py: change to comply with PEP440 local version

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=load_version() + "-huddly",
+    version=load_version() + "+huddly",
 
     description='Conan C/C++ package manager',
     long_description=generate_long_description_file(),
